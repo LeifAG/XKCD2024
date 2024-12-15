ffcf63a2759b3df2ad843d3528f08f8c81ac7c91
unknown <leif.hammar@gymnasium.ax>
2024-12-15 21:49:51 +0200

    js funk

--- js/comic.js
+++ js/comic.js
@@ -1,16 +1,34 @@
 //Definierar globala variabler för nuvarande och största comic nummer
-var maxComic = -1;
-var currentComic = -1;
+let maxComic = -1;
+let currentComic = -1;
 
 window.onload = function(){
     //hämta senaste comic
     getComic('latest');
     //sätter funktionalitet för nav knappar
-    document.getElementById('first').addEventListener('click',function(){
-        if(currentComic!=1){
+    document.getElementById('first').addEventListener('click', function() {
+        if (currentComic != 1) {
             getComic(1);
         }
-    })
+    });
+    document.getElementById('prev').addEventListener('click', function() {
+        if (currentComic > 1) {
+            getComic(currentComic - 1);
+        }
+    });
+    document.getElementById('random').addEventListener('click', function() {
+        let randomComic = Math.floor(Math.random() * maxComic) + 1;
+        getComic(randomComic);
+    });    
+    document.getElementById('next').addEventListener('click', function() {
+        if (currentComic < maxComic) {
+            getComic(currentComic + 1);
+        }
+    });
+    document.getElementById('last').addEventListener('click', function() {
+        getComic('latest');
+    });
+
 }
 
 function getComic(which){
@@ -31,7 +49,6 @@ function getComic(which){
                 maxComic=data.num;
             }
             //Skicka json data för behandling till DOM
-            console.log(data);
             appendComic(data);
         })
         .catch(function(error){
@@ -41,5 +58,32 @@ function getComic(which){
 }
 
 function appendComic(data){
-    
+    //Hämtar och tömmer main comic div
+    let comic = document.getElementById('comic');
+    comic.innerHTML="";
+
+    //Skapar och lägger till titeln till dokumentet
+    let titel = document.createElement('H3');
+    titel.innerHTML = data.title;
+    comic.appendChild(titel);
+
+    //Skapar och lägger till datumet till dokumentet
+    let datum = new Date(data.year,data.month-1,data.day);
+    let datumtag = document.createElement('time');
+    datumtag.innerHTML = datum.toLocaleDateString();
+    comic.appendChild(datumtag);
+
+    //Skapar och lägger till bilden samt en caption
+    // till dokumentet inom ett figure element
+    let figure = document.createElement('figure');
+    let img = document.createElement('img');
+    img.src=data.img;
+    let cap = document.createElement('figcaption');
+    cap.innerHTML = "Nummer: "+data.num;
+    figure.appendChild(img);
+    figure.appendChild(cap);
+    comic.appendChild(figure);
+
+    //Uppdaterar nuvarande comic nummer
+    currentComic=data.num;
 }
